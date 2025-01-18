7af3040aec657c03ee922e63bc31c2f51243f7ad
h0ttab <email@example.com>
2025-01-18 15:04:38 +0300

    Добавлено описание для функции формирования отчёта
    Добавлена документация (docstring) для функции формирования отчёта.

--- modules/report.py
+++ modules/report.py
@@ -7,14 +7,29 @@ env = Environment(loader=FileSystemLoader('./'))
 template = env.get_template('report_template.j2')
 
 def compose_report(weather_data: dict) -> dict:
+    """
+    Формирует текстовый отчёт на основе шаблона Jinja2
+
+    Входные данные представляют собой предварительно обработанный словарь с данными о погоде, сгруппированными по дням.
+    В функции предусмотрена валидация на наличие данных в отчёте, чтобы избежать появление "пустых" дней.
+
+    Args:
+        weather_data (dict): Словарь с данными об облачности, времени захода Солнца, освещённости и фазе Луны
+
+    Returns:
+        dict: Словарь со статусом формирования отчёта (error или success) и сообщением, которое в случае
+            успешного формирования отчёта содержит сам отчёт, или же с сообщением об ошибке, если
+            отчёт не был сформирован.
+    """
     current_time = str(datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
     is_data_present = False
 
-    # Проверяем, есть ли данные в отчёте
+    # Проверяем, есть ли данные для отчёта
     for day in weather_data.keys():
         if weather_data[day]["date_time"]:
             is_data_present = True
-
+    
+    # Если есть, то формируем отчёт
     if is_data_present:
         rendered_template = template.render(weather_data=weather_data, current_time=current_time, CLOUDINESS_FILTER=CLOUDINESS_FILTER)
         log(event_type="INFO", message="Сформирован отчёт", data=rendered_template)
